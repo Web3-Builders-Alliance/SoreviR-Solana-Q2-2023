a8df0b6000ec4e367ddfa5ac00a5a65dd7b307cc
SoreviR <rob.riveros.es@gmail.com>
2023-06-04 18:53:59 +0200

    deposit and withdraw spl test done

--- cluster1/wba_vault/tests/wba_vault.ts
+++ cluster1/wba_vault/tests/wba_vault.ts
@@ -1,5 +1,5 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, BN } from "@project-serum/anchor";
 import { WbaVault, IDL } from "../target/types/wba_vault";
 import {
   Keypair,
@@ -13,6 +13,7 @@ import {
   createMint,
   getOrCreateAssociatedTokenAccount,
   mintToChecked,
+  mintTo,
 } from "@solana/spl-token";
 import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
 
@@ -130,6 +131,171 @@ describe("wba_vault", () => {
       https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
     } catch (e) {
       console.error(e);
+    }
+  });
+
+  it("Deposit SPL", async () => {
+    // Add your test here.
+    //Create a new mint account with our wallet as init payer, mint authority and freeze authority
+    let mint = await createMint(
+      provider.connection,
+      keypair,
+      keypair.publicKey,
+      keypair.publicKey,
+      6
+    );
+    console.log("Mint ID: ", mint.toString());
+
+    //create our associated token account
+    let ataFrom = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    console.log("ATA From: ", ataFrom.address.toString());
+
+    //create the recipient token account
+    let ataTo = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      keypair,
+      mint,
+      vaultAuth,
+      true
+    );
+    console.log("ATA To: ", ataTo.address.toString());
+
+    //Mint SPL Tokens to our account (ATA)
+    let tx = await mintTo(
+      provider.connection,
+      keypair,
+      mint,
+      ataFrom.address,
+      keypair,
+      LAMPORTS_PER_SOL * 2
+    );
+
+    //Log our mint transaction
+    console.log("Token minted: ", tx);
+
+    try {
+      const txhash = await program.methods
+        .depositSpl(new BN(LAMPORTS_PER_SOL * 1))
+        .accounts({
+          owner: keypair.publicKey,
+          vaultState: vaultState.publicKey,
+          vaultAuth: vaultAuth,
+          systemProgram: SystemProgram.programId,
+          ownerAta: ataFrom.address,
+          vaultAta: ataTo.address,
+          tokenMint: mint,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        })
+        .signers([keypair])
+        .rpc();
+      console.log(`Success! Check out your TX here: 
+      https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+      let accountBalance = (
+        await provider.connection.getTokenAccountBalance(ataTo.address)
+      ).value.amount;
+      console.log("\nSPL Balance: ", accountBalance);
+    } catch (e) {
+      console.error(`Oops, something went wrong: ${e}`);
+    }
+  });
+
+  it("Withdraw SPL", async () => {
+    // Add your test here.
+    //Create a new mint account with our wallet as init payer, mint authority and freeze authority
+    let mint = await createMint(
+      provider.connection,
+      keypair,
+      keypair.publicKey,
+      keypair.publicKey,
+      6
+    );
+    console.log("Mint ID: ", mint.toString());
+
+    //create our associated token account
+    let ataFrom = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    console.log("ATA From: ", ataFrom.address.toString());
+
+    //create the recipient token account
+    let ataTo = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      keypair,
+      mint,
+      vaultAuth,
+      true
+    );
+    console.log("ATA To: ", ataFrom.address.toString());
+
+    //Mint SPL Tokens to our account (ATA)
+    let tx = await mintTo(
+      provider.connection,
+      keypair,
+      mint,
+      ataFrom.address,
+      keypair,
+      LAMPORTS_PER_SOL * 2
+    );
+
+    //Log our mint transaction
+    console.log("Token minted: ", tx);
+
+    try {
+      const txhash = await program.methods
+        .depositSpl(new BN(LAMPORTS_PER_SOL * 1))
+        .accounts({
+          owner: keypair.publicKey,
+          vaultState: vaultState.publicKey,
+          vaultAuth: vaultAuth,
+          systemProgram: SystemProgram.programId,
+          ownerAta: ataFrom.address,
+          vaultAta: ataTo.address,
+          tokenMint: mint,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        })
+        .signers([keypair])
+        .rpc();
+      console.log(`\nSPL Deposit Success! Check out your TX here: 
+      https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+      let accountBalance = (
+        await provider.connection.getTokenAccountBalance(ataTo.address)
+      ).value.amount;
+      console.log("SPL Balance: ", accountBalance);
+    } catch (e) {
+      console.error(`Oops, something went wrong: ${e}`);
+    }
+
+    try {
+      const txhash = await program.methods
+        .withdrawSpl(new BN(LAMPORTS_PER_SOL * 1))
+        .accounts({
+          owner: keypair.publicKey,
+          vaultState: vaultState.publicKey,
+          vaultAuth: vaultAuth,
+          systemProgram: SystemProgram.programId,
+          ownerAta: ataFrom.address,
+          vaultAta: ataTo.address,
+          tokenMint: mint,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        })
+        .signers([keypair])
+        .rpc();
+      console.log(`\n\nSPL Withdraw Success! Check out your TX here: 
+      https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+      let accountBalance = (
+        await provider.connection.getTokenAccountBalance(ataTo.address)
+      ).value.amount;
+      console.log("SPL Balance: ", accountBalance);
+    } catch (e) {
+      console.error(`Oops, something went wrong: ${e}`);
     }
   });
 });
